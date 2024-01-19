58829b93ead2be84b382b7203405d0da08b925c6
Venzelland <pavielvolodko1997@mail.ru>
2024-01-19 20:17:50 +0300

    11 lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,41 @@
 import React from 'react';
 import './App.css';
-import {Technologies} from "./components/Technologies";
-import {Header} from "./components/Header";
-import Footer from "./components/Footer";
 
 
 const App = () => {
     return (
-        <div>
-            <Header title={"NEW BODY"}/>
-            <Technologies titleForBody={"New Footer"}/>
-            <Footer/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src="https://img.freepik.com/free-vector/bird-colorful-logo-gradient-vector_343694-1365.jpg?size=338&ext=jpg&ga=GA1.1.632798143.1705536000&semt=ais"
+                    alt=""/>
+            </header>
+            <nav className="nav">
+                <div><a>Profile</a></div>
+                <div><a>Message</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+            </nav>
+            <div className='content'>
+                <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg" alt=""/>
+                <div>
+                    ava + discription
+                </div>
+                <div>
+                    my post
+                </div>
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+
         </div>
     );
 }
